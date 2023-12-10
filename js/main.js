00dbfc75f781333249003e217823920c08c7522a
Slava_Frondend <sidorov-vv3@gmail.com>
2023-12-11 01:18:04 +0800

    Added a js block for tabs

--- js/main.js
+++ js/main.js
@@ -51,3 +51,49 @@ const swiper = new Swiper('.swiper', {
     prevEl: '#sliderPrev',
   },
 });
+
+
+// Tabs
+const tabsBtns = document.querySelectorAll('[data-tab]');
+const tabsProductsAll = document.querySelectorAll('[data-tab-value]');
+
+for (let btn of tabsBtns) {
+
+  btn.addEventListener('click', function() {
+
+    // Убираю активный класс у всех кнопок
+    for (let btn of tabsBtns) {
+      btn.classList.remove('tab-controls__btn--active')
+    } 
+
+    // Добавляю активный класс к текущей кнопке
+    this.classList.add('tab-controls__btn--active');
+
+    // Отображаю нужные товары и скрываю все остальные товары 
+    for (let product of tabsProductsAll) {
+
+      // Проверяю на отображение всех товаров
+      if (this.dataset.tab === 'All') {
+
+        product.classList.remove('none');
+
+      } else {
+        
+        if (product.dataset.tabValue === this.dataset.tab) {
+          
+          product.classList.remove('none');
+
+        } else {
+
+          product.classList.add('none');
+          
+        }
+      
+      }
+    }
+
+    // update свайпер 
+    swiper.update();
+
+  })
+}
